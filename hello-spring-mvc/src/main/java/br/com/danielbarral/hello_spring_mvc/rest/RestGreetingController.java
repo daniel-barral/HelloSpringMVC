6819ce65de6f3a9bda3267ba47a15c6600e2cdc7
daniel-barral <danielbarral@gmail.com>
2016-05-16 01:41:06 -0300

    Renamed GreetingControllerRest to RestGreetingController, moved to rest folder and changed request mapping.

--- hello-spring-mvc/src/main/java/br/com/danielbarral/hello_spring_mvc/rest/RestGreetingController.java
+++ hello-spring-mvc/src/main/java/br/com/danielbarral/hello_spring_mvc/rest/RestGreetingController.java
@@ -1,4 +1,4 @@
-package br.com.danielbarral.hello_spring_mvc;
+package br.com.danielbarral.hello_spring_mvc.rest;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -7,13 +7,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.danielbarral.hello_spring_mvc.Greeting;
+
 @RestController
-public class GreetingControllerRest {
+public class RestGreetingController {
 	
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 	
-    @RequestMapping("/greetingRest")
+    @RequestMapping("/rest/greeting")
     public Greeting greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
